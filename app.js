580d777e0e0dd1a0397bba169ce0c2514bbe0bd7
Aziz Tovbaev <aziz.tovbaev@outlook.com>
2016-12-09 02:36:45 -0800

    add final changes

--- app.js
+++ app.js
@@ -4,6 +4,9 @@
 
 
 $(document).ready(function() {
+
+	// ADD LINK
+
 	$('.js-like').on('click', function(event){
 		event.preventDefault();
 
@@ -11,4 +14,30 @@ $(document).ready(function() {
 		.closest('.news-item')
 		.addClass('is-liked');
 	});
+
+
+	// ADD LINK;
+
+	$('.js-add-link').on('click', function(event){
+		event.preventDefault();
+
+		$('.js-form').toggleClass('is-visible');
+	});
+
+	// SHOW MODAL
+
+	$('.js-show-modal').on('click', function(event){
+		event.preventDefault();
+
+		$('.js-modal').toggleClass('is-visible');
+		$('.js-modal-overlay').toggleClass('is-visible');
+	});
+
+	$('.js-modal-overlay').on('click', function(event){
+		event.preventDefault();
+
+		$('.js-modal').removeClass('is-visible');
+		$('.js-modal-overlay').removeClass('is-visible');
+	});
+
 });
